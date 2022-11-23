42db628ca2e228ccbdfc55181ecbcced36566341
jinxin <jinxin@hsmap.com>
2022-11-23 15:04:47 +0800

    chore: 添加swagger3

---
+++ demo/src/main/java/com/example/clouddemo/config/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.example.clouddemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 说明：Swagger 拦截配置
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    /**
+     * swagger 拦截配置
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.
+                addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+    /**
+     * swagger 拦截配置
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/swagger-ui/")
+                .setViewName("forward:/swagger-ui/index.html");
+    }
+}
--- demo/src/main/java/com/example/clouddemo/controller/PmsBrandController.java
+++ demo/src/main/java/com/example/clouddemo/controller/PmsBrandController.java
@@ -7,7 +7,6 @@ import com.example.clouddemo.service.PmsBrandService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +16,7 @@ import java.util.List;
  * 品牌管理Controller
  * Created by macro on 2019/4/19.
  */
-@Controller
+@RestController
 @RequestMapping("/brand")
 public class PmsBrandController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
